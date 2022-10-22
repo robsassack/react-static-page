11ec92cc89f7119dd8d7cfba3d86119eb54fa69a
Rob Sassack <rsassack25@gmail.com>
2022-10-21 21:01:23 -0400

    Removed CDN link

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+const navbar = (
+  <nav>
+    <h1>Brand</h1>
+    <ul>
+      <li>Pricing</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+)
+
+function App() {
+  return (
+    navbar
+  );
+}
+
+export default App;
