5b273b37e6db29d1c431103c964260136d5c71c5
Rob Sassack <rsassack25@gmail.com>
2022-10-24 16:08:35 -0400

    Moved components to components directory

--- src/App.js
+++ src/App.js
@@ -1,31 +1,6 @@
 import "./App.css";
-
-function Header() {
-  return (
-    <header className="header">
-      <div className="logo-container">
-        <img src='./react-logo.png' alt='React logo' className="react-logo" />
-        <p>ReactFacts</p>
-      </div>
-      <p className="header-text">React Course - Project 1</p>
-    </header>
-  );
-}
-
-function Content() {
-  return (
-    <div className="content">
-      <h1>Fun facts about React</h1>
-      <ul className="content-list">
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100K stars on GitHub</li>
-        <li>Is maintained by Facebook</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
-    </div>
-  );
-}
+import Header from "./components/Header";
+import Content from "./components/Content";
 
 function App() {
   return (
