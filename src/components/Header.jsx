718927c6395ba51de924d39f1ee0aa9c1e39fa20
Rob Sassack <rsassack25@gmail.com>
2022-10-25 20:10:51 -0400

    Changed main font

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className="header">
       <div className="logo-container">
-        <img src='./react-logo.png' alt='React logo' className="react-logo" />
+        <img src='react-logo.png' alt='React logo' className="react-logo" />
         <p>ReactFacts</p>
       </div>
       <p className="header-text">React Course - Project 1</p>
