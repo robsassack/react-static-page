b09dec34d3fae9a0260c70d5d1fd680107baa462
Rob Sassack <rsassack25@gmail.com>
2022-10-25 20:33:24 -0400

    Header CSS tweaks

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 function Header() {
   return (
-    <header className="header">
+    <nav>
       <div className="logo-container">
         <img src='react-logo.png' alt='React logo' className="react-logo" />
         <p>ReactFacts</p>
       </div>
       <p className="header-text">React Course - Project 1</p>
-    </header>
+    </nav>
   );
 }
 
